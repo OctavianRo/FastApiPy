aa2bc744f1da1a08c5a3a7d8375b0bb41bc144d2
Octavian Rosca <octavian.rosca7@gmail.com>
2022-12-17 19:19:10 +0000

    added pydantic validation to api

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# we are going to validate the data that we are receiving here
+class Post(BaseModel):
+    title: str  
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -12,8 +21,8 @@ def get_posts():
     return {"data":"This is your post"}
 
 @app.post("/createposts")
-def create_post(payload: dict = Body(...)):
-    print(payload) 
-    return {"new_post" : f"title {payload['title']} content: {payload['content']}"}
-
+def create_post(post: Post): # we are going to validate the data based on the pydantic model
+    print(post)
+    print(post.dict())
+    return {"data": post}
 
