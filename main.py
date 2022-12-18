cd054bc064728b0299a87cd9b1a66a26e3597691
Octavian Rosca <octavian.rosca7@gmail.com>
2022-12-18 21:09:52 +0000

    add update crud operation

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ def get_post(id: int): # this is a path parameter
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     index = find_index_post(id)
-
     if index == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist") 
     my_posts.pop(index)
@@ -61,5 +60,11 @@ def delete_post(id: int):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int): # we are going to convert it to an int
-    pass
+def update_post(id: int, post: Post): # we are going to convert it to an int
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
