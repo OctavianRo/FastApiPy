f65e464c3835c6978b79d2927c5f3fdde4e4938a
Octavian Rosca <octavian.rosca7@gmail.com>
2022-12-23 13:22:52 +0000

    update posts in db

--- app/main.py
+++ app/main.py
@@ -81,11 +81,9 @@ def get_post(id: int): # this is a path parameter
 # returning 404 on error
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    
     cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id), ))
     deleted_post = cursor.fetchone()
     conn.commit()
-
     if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist") 
 
@@ -94,11 +92,11 @@ def delete_post(id: int):
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post): # we are going to convert it to an int
-    index = find_index_post(id)
-    if index == None:
+    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    updated_post=cursor.fetchone()
+
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+
+    return {"data": updated_post}
 
