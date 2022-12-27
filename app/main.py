0ce09049eb28ff652a605953b53873ba703e81e2
Octavian Rosca <octavian.rosca7@gmail.com>
2022-12-27 20:44:15 +0000

    remove data keyword

--- app/main.py
+++ app/main.py
@@ -7,20 +7,12 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session 
-
-# need to debug
-from . import models  
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# we are going to validate the data that we are receiving here
-class Post(BaseModel):
-    title: str  
-    content: str
-    published: bool = True
 
 # keep trying to connect ot the database 
 while True:
@@ -59,35 +51,34 @@ def find_index_post(id):
 def root():
     return {"message": "Welcome to the API"}
 
-
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return {"data": posts}
-
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
-    return {"data":posts}
+    return posts
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post, db: Session = Depends(get_db)): # we are going to validate the data based on the pydantic model
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)): # we are going to validate the data based on the pydantic model
     # # sanitizing inputs with the %s method 
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # # we have to commit these changes to db
     # conn.commit()
+
     new_post = models.Post(
         **post.dict()
     )
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
 @app.get("/posts/{id}")
@@ -99,7 +90,7 @@ def get_post(id: int, db: Session = Depends(get_db)): # this is a path parameter
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return {"post_detail": post}
+    return post
 
 # returning 404 on error
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -118,7 +109,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)): # we are going to convert it to an int
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)): # we are going to convert it to an int
     # cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     # updated_post=cursor.fetchone()
 
@@ -132,5 +123,5 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)): # w
     
     db.commit()
 
-    return {"data": post_query.first()}
+    return post_query.first()
 
