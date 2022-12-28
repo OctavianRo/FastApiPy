119a1b79956b70c3277df5c080d0ac84b5ea50f8
Octavian Rosca <octavian.rosca7@gmail.com>
2022-12-28 19:13:05 +0000

    add updated schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,18 +1,52 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
+
+from pydantic.types import conint
+
 
 class PostBase(BaseModel):
-    title: str  
+    title: str
     content: str
     published: bool = True
+
 
 class PostCreate(PostBase):
     pass
 
-class Post(PostBase):
-    id:int
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
 
-    # dict issue
     class Config:
         orm_mode = True
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
+
+
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
