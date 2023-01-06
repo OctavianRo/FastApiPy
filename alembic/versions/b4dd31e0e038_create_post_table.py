22dc3e004e5c8dedb5c09dcf1cfa37d465c41e26
Octavian Rosca <octavian.rosca7@gmail.com>
2023-01-06 21:44:10 +0000

    add alembic

---
+++ alembic/versions/b4dd31e0e038_create_post_table.py
@@ -0,0 +1,24 @@
+"""create post table
+
+Revision ID: b4dd31e0e038
+Revises: 
+Create Date: 2023-01-06 21:40:01.719038
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4dd31e0e038'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
