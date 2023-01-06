0ad5ec4df550ffcb928f202222140ba81638e247
Octavian Rosca <octavian.rosca7@gmail.com>
2023-01-06 16:11:35 +0000

    add updated models for vote

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from .database import Base
-
+from sqlalchemy.orm import relationship
 
 # define the model
 class Post(Base):
@@ -14,6 +14,10 @@ class Post(Base):
     content = Column(String, nullable=False)
     published=Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    # set up the foreign key constraint
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable = False)
+    owner = relationship("User") 
+
 
 class User(Base):
     __tablename__ = "users"
@@ -22,5 +26,10 @@ class User(Base):
     password=Column(String, nullable=False)
     id = Column(Integer, primary_key=True, nullable = False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key = True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key = True)
 
