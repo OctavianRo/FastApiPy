7e10e938d5f5da8d1316d66d29587c4055ae9ba6
Octavian Rosca <octavian.rosca7@gmail.com>
2023-01-14 11:41:08 +0000

    tidied post router

--- app/routers/post.py
+++ app/routers/post.py
@@ -5,12 +5,10 @@ from sqlalchemy import func
 from .. import models, schemas, oauth2
 from ..database import get_db
 
-
 router = APIRouter(
     prefix="/posts",
     tags=['Posts']
 )
-
 
 @router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
