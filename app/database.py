6efbc578865287353ee32bf9ed97012a819f7e32
Octavian Rosca <octavian.rosca7@gmail.com>
2023-01-14 17:28:47 +0000

    cleaned database fiel

--- app/database.py
+++ app/database.py
@@ -7,7 +7,6 @@ from psycopg2.extras import RealDictCursor
 from .config import settings
 
 
-# SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:Trumpet79?@localhost/fastapi"
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
