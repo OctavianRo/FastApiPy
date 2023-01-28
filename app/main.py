c449094be6c1b58419c0e2f707768e055d3ba502
Octavian Rosca <octavian.rosca7@gmail.com>
2023-01-28 17:06:11 +0000

    update to main

--- app/main.py
+++ app/main.py
@@ -17,13 +17,11 @@ app = FastAPI()
 
 origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins
+app.add_middleware(CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
-)
+    allow_headers=["*"])
 
 app.include_router(post.router)
 app.include_router(user.router)
